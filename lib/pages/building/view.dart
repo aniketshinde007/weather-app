117fb98deb547e8b171a61bd85de57adb9c8069e
Aniket Shinde <anikets13499@gmail.com>
2023-12-25 10:56:20 +0530

    new: Building page

---
+++ lib/pages/building/view.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gap/gap.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:weather/constants/svgs.dart';
+
+class BuildingView extends ConsumerWidget {
+  final Widget appbarTitle;
+  const BuildingView({super.key, required this.appbarTitle,});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar.large(
+            title: appbarTitle,
+          ),
+          const SliverToBoxAdapter(
+            child: Gap(40),
+          ),
+          SliverToBoxAdapter(
+            child: SvgPicture.string(
+              SVGs.buildingBlocks,
+              height: MediaQuery.of(context).size.width / 2,
+              width: MediaQuery.of(context).size.width / 2,
+            ),
+          ),
+          const SliverToBoxAdapter(
+            child: Gap(40),
+          ),
+          SliverToBoxAdapter(
+            child: Center(
+              child: Text(
+                'Page under development',
+                style: Theme.of(context).textTheme.headlineSmall,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
